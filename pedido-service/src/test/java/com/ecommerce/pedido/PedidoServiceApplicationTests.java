d614c12cfb29688c5b8fa887c7844c4c90370284
Juan Rodriguez <juanprodrigues.33@gmail.com>
2026-07-01 18:57:09 -0300

    feat: test solution by profile

--- pedido-service/src/test/java/com/ecommerce/pedido/PedidoServiceApplicationTests.java
+++ pedido-service/src/test/java/com/ecommerce/pedido/PedidoServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.ecommerce.pedido;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = PedidoServiceApplication.class)
+@ActiveProfiles("test")
 class PedidoServiceApplicationTests {
 
     @Test
